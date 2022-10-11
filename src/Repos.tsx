b9c32e59656029d5aa88630eefef7fbd3e5aadb7
Frank Blendinger <yogan@zogan.de>
2022-10-11 13:24:28 +0200

    Load list of repos from GitHub and render as list
    Plain and simple, but gets the job done.
    
    What we already have:
    
        - rendering of repo name and number of stars
        - loading indicator
        - basic error handling
        - rudimentary types for API response
    
    What needs to be done:
    
        - tests!
        - pass correct date as query param so that only repos created in the
          last week are queried
        - extract a component for a single repo, so that more info can be
          shown (also separation of concerns / SRP)

--- src/Repos.tsx
+++ src/Repos.tsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react"
+
+const BASE_URL = 'https://api.github.com/search/repositories'
+
+// TODO: pass current year/month
+const PARAMS = 'q=created:>2022-10-01&sort=stars&order=desc'
+
+type ApiResponse = {
+    items: Repository[]
+}
+
+type Repository = {
+    name: string
+    stargazers_count: number
+}
+
 function Repos() {
+    const [data, setData] = useState<Repository[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const loadData = async () => {
+            setLoading(true)
+            setError(null)
+            try {
+                const response = await fetch(`${BASE_URL}?${PARAMS}`)
+                if (!response.ok) {
+                    throw new Error(`HTTP error (status ${response.status})`)
+                }
+                const json = await response.json() as unknown as ApiResponse
+                setData(json.items)
+                // console.log(json.items)
+            } catch (err) {
+                setError((err as Error).message)
+                setData([])
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadData()
+    }, [])
+
     return (
-        <p>Repos! Stars! Profit!</p>
+        <>
+            {data.length > 0
+                ? <ul>{data.map(d => <Repo repo={d} key={d.name} />)}</ul>
+                : null
+            }
+
+            {loading
+                ? <span>Loading…</span>
+                : null}
+
+            {error !== null
+                ? <span>Could not load repositories. 😢</span>
+                : null
+            }
+        </>
     )
 }
+
+const Repo = ({ repo }: { repo: Repository }) =>
+    <li>{repo.name} ({repo.stargazers_count} ⭐s)</li>
 
 export default Repos
