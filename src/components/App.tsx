04a1ce4188dc68291ebeb8cd571ea3b0fbd1f09a
Frank Blendinger <yogan@zogan.de>
2022-11-03 13:17:35 +0100

    Consistent four space indentation for all files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,42 +6,42 @@ import RepositoryWrapper from './RepositoryWrapper'
 import './App.css'
 
 function App() {
-  const [onlyFavorites, setOnlyFavorites] = useState(false)
-  const toggleOnlyFavorites = () => setOnlyFavorites(!onlyFavorites)
+    const [onlyFavorites, setOnlyFavorites] = useState(false)
+    const toggleOnlyFavorites = () => setOnlyFavorites(!onlyFavorites)
 
-  const [language, setLanguage] = useState('all')
+    const [language, setLanguage] = useState('all')
 
-  const onLanguageChange: LanguageDropdownProps['onChange']
-    = (event) => setLanguage(event.target.value)
+    const onLanguageChange: LanguageDropdownProps['onChange']
+        = (event) => setLanguage(event.target.value)
 
-  return (
-    <>
-      <header>
-        <a href="https://reactjs.org" target="_blank">
-          <img src="/react.svg" className="logo react" alt="React logo" />
-        </a>
-        <h1>GitHub Repository Finder</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </header>
-      <main>
-        <div className='toolbar'>
-          <LanguageDropdown
-            onChange={onLanguageChange}
-          />
-          <FavoritesFilterButton
-            showOnlyFavorites={onlyFavorites}
-            toggleOnlyFavorites={toggleOnlyFavorites}
-          />
-        </div>
-        <RepositoryWrapper
-          language={language}
-          showOnlyFavorites={onlyFavorites}
-        />
-      </main>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <a href="https://reactjs.org" target="_blank">
+                    <img src="/react.svg" className="logo react" alt="React logo" />
+                </a>
+                <h1>GitHub Repository Finder</h1>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src="/vite.svg" className="logo" alt="Vite logo" />
+                </a>
+            </header>
+            <main>
+                <div className='toolbar'>
+                    <LanguageDropdown
+                        onChange={onLanguageChange}
+                    />
+                    <FavoritesFilterButton
+                        showOnlyFavorites={onlyFavorites}
+                        toggleOnlyFavorites={toggleOnlyFavorites}
+                    />
+                </div>
+                <RepositoryWrapper
+                    language={language}
+                    showOnlyFavorites={onlyFavorites}
+                />
+            </main>
+        </>
+    )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,10 @@ import './index.css'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    </React.StrictMode>
 )
